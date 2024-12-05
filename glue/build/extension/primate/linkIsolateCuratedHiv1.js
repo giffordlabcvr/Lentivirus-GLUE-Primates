3183c47a3d534589efef56022de6a1ab7125870f
Robert Gifford <robjgiff@gmail.com>
2024-12-05 17:00:42 +0000

    Refactor

--- glue/build/extension/primate/linkIsolateCuratedHiv1.js
+++ glue/build/extension/primate/linkIsolateCuratedHiv1.js
@@ -1,16 +1,19 @@
 // Array of configurations for the different sequence groups
 var sequenceConfigs = [
-    { sourceName: 'ncbi-curated-primate-subtype-a', subtype: 'A' },
-    { sourceName: 'ncbi-curated-primate-subtype-b', subtype: 'B' },
-    { sourceName: 'ncbi-curated-primate-subtype-c', subtype: 'C' },
-    { sourceName: 'ncbi-curated-primate-subtype-d', subtype: 'D' },
-    { sourceName: 'ncbi-curated-primate-subtype-f1', subtype: 'F1' },
-    { sourceName: 'ncbi-curated-primate-subtype-g', subtype: 'G' },
-    { sourceName: 'ncbi-curated-primate-crf01', subtype: 'CRF01' },
-    { sourceName: 'ncbi-curated-primate-crf02', subtype: 'CRF02' },
+    { sourceName: 'ncbi-curated-primate-subtype-a', clade: 'M', subtype: 'A' },
+    { sourceName: 'ncbi-curated-primate-subtype-b', clade: 'M', subtype: 'B' },
+    { sourceName: 'ncbi-curated-primate-subtype-c', clade: 'M', subtype: 'C' },
+    { sourceName: 'ncbi-curated-primate-subtype-d', clade: 'M', subtype: 'D' },
+    { sourceName: 'ncbi-curated-primate-subtype-f1', clade: 'M', subtype: 'F1' },
+    { sourceName: 'ncbi-curated-primate-subtype-g', clade: 'M', subtype: 'G' },
+    { sourceName: 'ncbi-curated-primate-crf01', clade: 'M', subtype: 'CRF01' },
+    { sourceName: 'ncbi-curated-primate-crf02', clade: 'M', subtype: 'CRF02' },
     { sourceName: 'ncbi-curated-primate-group-n', clade: 'N', subtype: 'NULL' },
     { sourceName: 'ncbi-curated-primate-group-o', clade: 'O', subtype: 'NULL' },
     { sourceName: 'ncbi-curated-primate-group-p', clade: 'P', subtype: 'NULL' }
+    { sourceName: 'ncbi-curated-primate-siv-cpz', clade: 'NULL', subtype: 'NULL' }
+    { sourceName: 'ncbi-curated-primate-group-p', clade: 'NULL', subtype: 'NULL' }
+
 ];
 
 // Loop through each configuration
@@ -35,7 +38,7 @@ _.each(sequenceConfigs, function(config) {
         glue.inMode("sequence/" + config.sourceName + "/" + seqId, function() {
             glue.command(["set", "field", "subgenus", 'Primate']);
             glue.command(["set", "field", "species", 'HIV-1']);
-            glue.command(["set", "field", "clade", config.clade || 'M']); // Default clade is 'M'
+            glue.command(["set", "field", "clade", config.clade]);
             glue.command(["set", "field", "subtype", config.subtype]);
         });
     });
